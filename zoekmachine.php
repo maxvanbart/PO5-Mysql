d66d5e2d03bb61e7b9c596cc91ff4fbfb99543a2
prullemand <37772860+prullemand@users.noreply.github.com>
2018-03-28 17:11:33 +0200

    Add files via upload
    Fix het gvd probleem. bij voornaam zoeken geeft hij alle films weer.

---
+++ zoekmachine.php
@@ -0,0 +1,20 @@
+<?php
+//including the database connection file
+include_once("includes/db_connect.php");
+?>
+ 
+<html>
+<head>    
+    <title>Film</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+ 
+<body>
+	<?php include "includes/menu.php" ?>
+	<h2>Zoekmachine</h2>
+	    <form id="gegevens" action="zoek.php" method="post">
+			<input type="text" name="zoek"></td>
+			<input type="submit" name="Submit" value="zoek"></td>
+    </form>
+</body>
+</html>
