b030dce242b2a26f1c3aec1dd0b59192bb17ae03
maxvanbart <36897805+maxvanbart@users.noreply.github.com>
2018-03-28 20:41:08 +0200

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -1,4 +1,5 @@
 <ul class="menu">
  <li><a href="homepage.php"><span>Home</span></a></li>
  <li><a href="gegevens.php"><span>Gegevens</span></a></li>
+ <li><a href="zoek.php"><span>Zoek</span></a></li>
 </ul>
